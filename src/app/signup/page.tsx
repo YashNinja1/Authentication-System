fe9e9a1a33dd20efe02e549f17a46b6713f9221e
Yash_Kh <yashkharche258@gmail.com>
2025-07-30 09:36:12 +0530

    nextjs framework ready with database connected and login and signup page frontend ready

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+//import { axios } from "axios";
+
+export default function SignUpPage() {
+  const [user, setUser] = React.useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const OnSignUp = async () => {};
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl">SignUp</h1>
+      <hr />
+      <label htmlFor="username">Username:</label>
+      <input
+        className="p-2 border border-gray-300 bg-white rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black  text-center text-sm"
+        id="username"
+        type="text"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+        placeholder="Enter Username"
+      />
+      <label htmlFor="email">Email:</label>
+      <input
+        className="p-2 border border-gray-300 bg-white rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black  text-center text-sm"
+        id="email"
+        type="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+        placeholder="Enter Email"
+      />
+      <label htmlFor="password">Password:</label>
+      <input
+        className="p-2 border border-gray-300 bg-white rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black  text-center text-sm"
+        id="password"
+        type="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+        placeholder="Enter Email"
+      />
+      <button
+        onClick={OnSignUp}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        SignUp
+      </button>
+      <Link href="/login">Visit Login</Link>
+    </div>
+  );
+}
---
+++ src/dbConfig/dbConfig.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+export async function connect() {
+  try {
+    mongoose.connect(process.env.MONGO_URL!);
+    const connection = mongoose.connection;
+    connection.on("connected", () => {
+      console.log("MongoDB connected successfully");
+    });
+    connection.on("error", (err) => {
+      console.log("Error Occured:" + err);
+      process.exit();
+    });
+  } catch (error) {
+    console.log("Something went Wrong");
+    console.log(error);
+  }
+}
